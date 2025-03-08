f1fc822458cc5ed71727080d1b752cdcb2afa8d6
VishnuKC26 <vishnukantchaudhary@gmail.com>
2025-03-09 00:48:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/twitter/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.twitter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.twitter.ui.theme.TwitterTheme
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(this)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TweetingApp()
+
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TwitterTheme {
+        Greeting("Android")
+    }
+}
